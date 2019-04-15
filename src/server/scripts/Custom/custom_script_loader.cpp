c9b9dab1e66c7da605210c1ff27985e3baaa4d6a
Baynar <stanislav118@abv.bg>
2019-04-15 03:14:01 +0300

    Fix Build

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -22,56 +22,9 @@
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// 10
-// 11
-// 12
-//TeleNPC2
-void AddSC_npc_teleport();
-// 13
-// 14
-// 15
-// 16
-// 17
-// 18
-// 19
-// 20
-// 21
-// 22
-// 23
-// 24
-// 25
-// 26
-// 27
-// 28
-// 29
-// 30
-// 31
-// 32
-// 33
-// 34
-// 35
-// 36
-// 37
-// 38
-// 39
-// 40
-
-void AddCustomScripts()
-{
     // 1
     // 2
     // 3
-    // TeleNPC2
-    AddSC_npc_teleport();
     // 4
     // 5
     // 6
@@ -81,6 +34,8 @@ void AddCustomScripts()
     // 10
     // 11
     // 12
+    //TeleNPC2
+    void AddSC_npc_teleport();
     // 13
     // 14
     // 15
